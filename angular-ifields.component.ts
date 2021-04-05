48be80470ab90e145782dbe0dd19f40ba6fb2011
mstein <mstein@cardknox.com>
2021-04-05 11:07:15 -0400

    moved all message actions

--- angular-ifields.component.ts
+++ angular-ifields.component.ts
@@ -88,7 +88,7 @@ export class AngularIfieldsComponent implements AfterViewInit, OnChanges {
     this.messagePoster = new MessagePoster(this.elementRef.nativeElement.children[0].contentWindow.postMessage.bind(this.elementRef.nativeElement.children[0].contentWindow),
       this.options, this.type, () => this.iFrameLoaded);
     window.addEventListener("message", this.onMessage);
-    this.ping();
+    this.messagePoster?.ping();
   }
 
   ngOnChanges(changes: SimpleChanges) {
@@ -120,176 +120,46 @@ export class AngularIfieldsComponent implements AfterViewInit, OnChanges {
         val.autoFormat !== oldVal.autoFormat ||
         val.autoFormatSeparator !== oldVal.autoFormatSeparator
       )
-        this.enableAutoFormat(val.autoFormatSeparator);
+        this.messagePoster?.enableAutoFormat(val.autoFormatSeparator);
     }
     if (val.autoSubmit) {
       if (
         val.autoSubmit !== oldVal.autoSubmit ||
         val.autoSubmitFormId !== oldVal.autoSubmitFormId
       )
-        this.enableAutoSubmit(val.autoSubmitFormId);
+        this.messagePoster?.enableAutoSubmit(val.autoSubmitFormId);
     }
-    if (val.enableLogging && !oldVal.enableLogging) this.enableLogging();
+    if (val.enableLogging && !oldVal.enableLogging)
+      this.messagePoster?.enableLogging();
     if (val.placeholder && val.placeholder !== oldVal.placeholder)
-      this.setPlaceholder(val.placeholder);
+      this.messagePoster?.setPlaceholder(val.placeholder);
     if (val.iFieldstyle !== oldVal.iFieldstyle)
-      this.setStyle(val.iFieldstyle);
+      this.messagePoster?.setStyle(val.iFieldstyle);
   }
   updateIssuerChanges(currentValue: string) {
-    this.updateIssuer(currentValue);
+    this.messagePoster?.updateIssuer(currentValue);
   }
   shouldUpdateIssuer() {
     return this.type === CVV_TYPE;
   }
   updateThreeDSChanges(val: ThreeDS, oldVal: ThreeDS) {
     if (val.enable3DS)
-      this.enable3DS(val.waitForResponse, val.waitForResponseTimeout);
+      this.messagePoster?.enable3DS(val.waitForResponse, val.waitForResponseTimeout);
     if (val.amount !== oldVal.amount || !oldVal.enable3DS)
-      this.update3DS(AMOUNT, val.amount);
+      this.messagePoster?.update3DS(AMOUNT, val.amount);
     if (val.month !== oldVal.month || !oldVal.enable3DS)
-      this.update3DS(MONTH, val.month);
+      this.messagePoster?.update3DS(MONTH, val.month);
     if (val.year !== oldVal.year || !oldVal.enable3DS)
-      this.update3DS(YEAR, val.year);
+      this.messagePoster?.update3DS(YEAR, val.year);
   }
 
   updateAccountChanges(currentValue: Account) {
     const newAccount = transformAccountData(currentValue);
-    this.setAccount(newAccount);
+    this.messagePoster?.setAccount(newAccount);
   }
 
   shouldUpdateThreeDS(currentValue: ThreeDS) {
     return this.type === CARD_TYPE && currentValue.enable3DS;
-  }
-
-  ping() {
-    var message = {
-      action: PING
-    };
-    this.logAction(PING);
-    this.postMessage(message);
-  }
-
-  setAccount(data: Account) {
-    var message = {
-      action: SET_ACCOUNT_DATA,
-      data
-    };
-    this.logAction(SET_ACCOUNT_DATA);
-    this.postMessage(message);
-  }
-
-  init() {
-    var message = {
-      action: INIT,
-      tokenType: this.type,
-      referrer: window.location.toString()
-    };
-    this.logAction(INIT);
-    this.postMessage(message);
-  }
-  getToken() {
-    var message = {
-      action: GET_TOKEN
-    };
-    this.logAction(GET_TOKEN);
-    this.tokenLoading = true;
-    this.postMessage(message);
-  }
-
-  enable3DS(waitForResponse?: boolean, waitForResponseTimeout?: number) {
-    var message = {
-      action: ENABLE3DS,
-      data: {
-        waitForResponse,
-        waitForResponseTimeout
-      }
-    };
-    this.logAction(ENABLE3DS);
-    this.postMessage(message);
-  }
-
-  update3DS(fieldName: string, value?: string | number) {
-    var message = {
-      action: UPDATE3DS,
-      data: {
-        fieldName,
-        value
-      }
-    };
-    this.logAction(UPDATE3DS);
-    this.postMessage(message);
-  }
-
-  updateIssuer(issuer: string) {
-    var message = {
-      action: UPDATE_ISSUER,
-      issuer
-    };
-    this.logAction(UPDATE_ISSUER);
-    this.postMessage(message);
-  }
-
-  setPlaceholder(data: string) {
-    var message = {
-      action: SET_PLACEHOLDER,
-      data
-    };
-    this.logAction(SET_PLACEHOLDER);
-    this.postMessage(message);
-  }
-
-  enableAutoFormat(formatChar?: string) {
-    var message = {
-      action: FORMAT,
-      data: {
-        formatChar
-      }
-    };
-    this.logAction(FORMAT);
-    this.postMessage(message);
-  }
-  enableLogging() {
-    var message = {
-      action: ENABLE_LOGGING
-    };
-    this.logAction(ENABLE_LOGGING);
-    this.postMessage(message);
-  }
-
-  enableAutoSubmit(formId?: string) {
-    var message = {
-      action: ENABLE_AUTO_SUBMIT,
-      data: {
-        formId
-      }
-    };
-    this.logAction(ENABLE_AUTO_SUBMIT);
-    this.postMessage(message);
-  }
-
-  setStyle(data: any) {
-    var message = {
-      action: STYLE,
-      data
-    };
-    this.logAction(STYLE);
-    this.postMessage(message);
-  }
-
-  focusIfield() {
-    var message = {
-      action: FOCUS
-    };
-    this.logAction(FOCUS);
-    this.postMessage(message);
-  }
-
-  clearIfield() {
-    var message = {
-      action: CLEAR_DATA
-    };
-    this.logAction(CLEAR_DATA);
-    this.postMessage(message);
   }
 
   onMessage = (e: MessageEvent) => {
@@ -325,27 +195,27 @@ export class AngularIfieldsComponent implements AfterViewInit, OnChanges {
     this.iFrameLoaded = true;
     if (this.account) {
       const newAccount = transformAccountData(this.account);
-      this.setAccount(newAccount);
+      this.messagePoster?.setAccount(newAccount);
     }
     if (this.type === CARD_TYPE && this.threeDS && this.threeDS.enable3DS) {
-      this.enable3DS(this.threeDS.waitForResponse, this.threeDS.waitForResponseTimeout);
-      this.update3DS(AMOUNT, this.threeDS.amount);
-      this.update3DS(MONTH, this.threeDS.month);
-      this.update3DS(YEAR, this.threeDS.year);
+      this.messagePoster?.enable3DS(this.threeDS.waitForResponse, this.threeDS.waitForResponseTimeout);
+      this.messagePoster?.update3DS(AMOUNT, this.threeDS.amount);
+      this.messagePoster?.update3DS(MONTH, this.threeDS.month);
+      this.messagePoster?.update3DS(YEAR, this.threeDS.year);
     }
-    this.init();
+    this.messagePoster?.init();
     if (this.type === CVV_TYPE && this.issuer)
-      this.updateIssuer(this.issuer);
+      this.messagePoster?.updateIssuer(this.issuer);
     if (this.options.placeholder)
-      this.setPlaceholder(this.options.placeholder);
+      this.messagePoster?.setPlaceholder(this.options.placeholder);
     if (this.options.enableLogging)
-      this.enableLogging();
+      this.messagePoster?.enableLogging();
     if (this.type === CARD_TYPE && this.options.autoFormat)
-      this.enableAutoFormat(this.options.autoFormatSeparator);
+      this.messagePoster?.enableAutoFormat(this.options.autoFormatSeparator);
     if (this.options.autoSubmit)
-      this.enableAutoSubmit(this.options.autoSubmitFormId);
+      this.messagePoster?.enableAutoSubmit(this.options.autoSubmitFormId);
     if (this.options.iFieldstyle)
-      this.setStyle(this.options.iFieldstyle);
+      this.messagePoster?.setStyle(this.options.iFieldstyle);
     this.load.emit();
   }
 
@@ -370,7 +240,7 @@ export class AngularIfieldsComponent implements AfterViewInit, OnChanges {
       isValid: data.isValid
     };
     if (data.isValid && !this.tokenValid && !this.tokenLoading) {
-      this.getToken();
+      this.messagePoster?.getToken();
     }
     if (!data.isValid) {
       this.tokenValid = false;
@@ -440,10 +310,6 @@ export class AngularIfieldsComponent implements AfterViewInit, OnChanges {
     if (this.options.enableLogging) {
       console.log(`IField ${this.type}: ${message}`);
     }
-  }
-
-  logAction(action: string) {
-    this.log(`Sending message ${action}`);
   }
 
   error(message: string) {
--- message-poster.ts
+++ message-poster.ts
@@ -1,8 +1,11 @@
 
-import { Options, OutMessage, PostDelegate } from "../typings";
-import { PING } from "./constants";
+import { Account, Options, OutMessage, PostDelegate } from "../typings";
+import { CLEAR_DATA, ENABLE3DS, ENABLE_AUTO_SUBMIT, ENABLE_LOGGING, FOCUS, FORMAT, GET_TOKEN, INIT, PING, SET_ACCOUNT_DATA, SET_PLACEHOLDER, STYLE, UPDATE3DS, UPDATE_ISSUER } from "./constants";
 
 export default class MessagePoster{
+
+  tokenLoading: boolean = false;
+
   constructor(private post: PostDelegate, private options: Options, private type: string, private iFrameLoaded: () => boolean) { }
   
   
@@ -31,6 +34,134 @@ export default class MessagePoster{
       action: PING
     };
     this.logAction(PING);
+    this.postMessage(message);
+  }
+
+  setAccount(data: Account) {
+    var message = {
+      action: SET_ACCOUNT_DATA,
+      data
+    };
+    this.logAction(SET_ACCOUNT_DATA);
+    this.postMessage(message);
+  }
+
+  init() {
+    var message = {
+      action: INIT,
+      tokenType: this.type,
+      referrer: window.location.toString()
+    };
+    this.logAction(INIT);
+    this.postMessage(message);
+  }
+  
+  getToken() {
+    var message = {
+      action: GET_TOKEN
+    };
+    this.logAction(GET_TOKEN);
+    this.tokenLoading = true;
+    this.postMessage(message);
+  }
+
+  enable3DS(waitForResponse?: boolean, waitForResponseTimeout?: number) {
+    var message = {
+      action: ENABLE3DS,
+      data: {
+        waitForResponse,
+        waitForResponseTimeout
+      }
+    };
+    this.logAction(ENABLE3DS);
+    this.postMessage(message);
+  }
+
+  update3DS(fieldName: string, value?: string | number) {
+    var message = {
+      action: UPDATE3DS,
+      data: {
+        fieldName,
+        value
+      }
+    };
+    this.logAction(UPDATE3DS);
+    this.postMessage(message);
+  }
+
+  updateIssuer(issuer: string) {
+    var message = {
+      action: UPDATE_ISSUER,
+      issuer
+    };
+    this.logAction(UPDATE_ISSUER);
+    this.postMessage(message);
+  }
+
+  setPlaceholder(data: string) {
+    var message = {
+      action: SET_PLACEHOLDER,
+      data
+    };
+    this.logAction(SET_PLACEHOLDER);
+    this.postMessage(message);
+  }
+
+  enableAutoFormat(formatChar?: string) {
+    var message = {
+      action: FORMAT,
+      data: {
+        formatChar
+      }
+    };
+    this.logAction(FORMAT);
+    this.postMessage(message);
+  }
+  
+  enableLogging() {
+    var message = {
+      action: ENABLE_LOGGING
+      //TODO: should this have a bool param
+    };
+    this.logAction(ENABLE_LOGGING);
+    this.postMessage(message);
+  }
+
+  enableAutoSubmit(formId?: string) {
+    var message = {
+      action: ENABLE_AUTO_SUBMIT,
+      data: {
+        formId
+      }
+    };
+    this.logAction(ENABLE_AUTO_SUBMIT);
+    this.postMessage(message);
+  }
+
+  setStyle(data: any) {
+    var message = {
+      action: STYLE,
+      data
+    };
+    this.logAction(STYLE);
+    this.postMessage(message);
+  }
+
+  //TODO: implement
+  focusIfield() {
+    var message = {
+      action: FOCUS
+    };
+    this.logAction(FOCUS);
+    this.postMessage(message);
+  }
+
+  //TODO: implement
+  clearIfield() {
+    var message = {
+      action: CLEAR_DATA
+    };
+    this.logAction(CLEAR_DATA);
     this.postMessage(message);
   }
 }
