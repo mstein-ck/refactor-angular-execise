f3804516b5a2de22bd2c3d236478059e8d6e0e16
mstein <mstein@cardknox.com>
2021-04-05 12:26:12 -0400

    moved onload function

--- angular-ifields.component.ts
+++ angular-ifields.component.ts
@@ -199,33 +199,6 @@ export class AngularIfieldsComponent implements AfterViewInit, OnChanges {
     }
   }
 
-  onLoad() {
-    this.iFrameLoaded = true;
-    if (this.account) {
-      const newAccount = transformAccountData(this.account);
-      this.messagePoster?.setAccount(newAccount);
-    }
-    if (this.type === CARD_TYPE && this.threeDS && this.threeDS.enable3DS) {
-      this.messagePoster?.enable3DS(this.threeDS.waitForResponse, this.threeDS.waitForResponseTimeout);
-      this.messagePoster?.update3DS(AMOUNT, this.threeDS.amount);
-      this.messagePoster?.update3DS(MONTH, this.threeDS.month);
-      this.messagePoster?.update3DS(YEAR, this.threeDS.year);
-    }
-    this.messagePoster?.init();
-    if (this.type === CVV_TYPE && this.issuer)
-      this.messagePoster?.updateIssuer(this.issuer);
-    if (this.options.placeholder)
-      this.messagePoster?.setPlaceholder(this.options.placeholder);
-    if (this.options.enableLogging)
-      this.messagePoster?.enableLogging();
-    if (this.type === CARD_TYPE && this.options.autoFormat)
-      this.messagePoster?.enableAutoFormat(this.options.autoFormatSeparator);
-    if (this.options.autoSubmit)
-      this.messagePoster?.enableAutoSubmit(this.options.autoSubmitFormId);
-    if (this.options.iFieldstyle)
-      this.messagePoster?.setStyle(this.options.iFieldstyle);
-  }
-
   onToken({ data }: any) {
     this.tokenLoading = false;
     if (data.result === ERROR) {
