0c0ae86bfa7b9ec02eb7c5c88074c10052d27f83
mstein <mstein@cardknox.com>
2021-04-05 10:47:46 -0400

    added ping to poster class

--- message-poster.ts
+++ message-poster.ts
@@ -1,6 +1,36 @@
 
-import { PostDelegate } from "../typings";
+import { Options, OutMessage, PostDelegate } from "../typings";
+import { PING } from "./constants";
 
 export default class MessagePoster{
-  constructor(private post: PostDelegate){}
+  constructor(private post: PostDelegate, private options: Options, private type: string, private iFrameLoaded: () => boolean) { }
+  
+  
+
+  postMessage(data: OutMessage) {
+    if (!this.iFrameLoaded() && data.action !== PING) {
+      this.log("Iframe not loaded");
+      this.ping();
+      return;
+    }
+    this.post(data, "*");
+  }
+
+  log(message: string) {
+    if (this.options.enableLogging) {
+      console.log(`IField ${this.type}: ${message}`);
+    }
+  }
+
+  logAction(action: string) {
+    this.log(`Sending message ${action}`);
+  }
+
+  ping() {
+    var message = {
+      action: PING
+    };
+    this.logAction(PING);
+    this.postMessage(message);
+  }
 }
